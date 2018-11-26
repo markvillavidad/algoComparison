f4fe634cf9ec7d6ff5af8c5fb4acece8933d2125
markvidad <markvillavidad@gmail.com>
2018-11-26 15:51:48 -0500

    Test Change ffdsfd

--- Algo1/Program.cs
+++ Algo1/Program.cs
@@ -6,7 +6,7 @@ namespace Algo1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bitch!");
         }
     }
 }
